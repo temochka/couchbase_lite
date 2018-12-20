4ee5b80e6a6816e7e31a142aed60decb3231b44d
Artem Chistyakov <tema@wildbit.com>
2018-12-20 16:10:24 -0500

    Add a failing test.

--- spec/couchbase_lite/server_spec.rb
+++ spec/couchbase_lite/server_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe CouchbaseLite::Server do
   subject(:server) { CouchbaseLite::Server.new { |_| db } }
   let(:port) { 4666 }
   let(:client_socket_factory) { CouchbaseLite::ReplicatorSocketFactory.new(server: false) }
+  let(:server_replicator) { server.replications.first }
 
   around(:example) do |ex|
     EM.run do
@@ -28,7 +29,7 @@ RSpec.describe CouchbaseLite::Server do
                                                  url: "ws://localhost:#{port}/db")
 
       expect { replicator.start }.
-        to run_until { replicator.status.level == :idle }.
+        to run_until { replicator.status.level == :idle && server_replicator.status.level == :idle }.
           with_timeout(5).
           and_then {
             expect(n1ql('SELECT *', db).run.to_a).to eq(n1ql('SELECT *', db_replica).run.to_a)
@@ -49,7 +50,7 @@ RSpec.describe CouchbaseLite::Server do
                                                  url: "ws://localhost:#{port}/db")
 
       expect { replicator.start }.
-        to run_until { replicator.status.level == :idle }.
+        to run_until { replicator.status.level == :idle && server_replicator.status.level == :idle }.
           with_timeout(5).
           and_then {
             expect(n1ql('SELECT *', db).run.to_a).to eq(n1ql('SELECT *', db_replica).run.to_a)
@@ -73,7 +74,7 @@ RSpec.describe CouchbaseLite::Server do
                                                  url: "ws://localhost:#{port}/db")
 
       expect { replicator.start }.
-        to run_until { replicator.status.level == :idle }.
+        to run_until { replicator.status.level == :idle && server_replicator.status.level == :idle }.
           with_timeout(5).
           and_then {
             doc = db_replica.get('42')
@@ -99,7 +100,8 @@ RSpec.describe CouchbaseLite::Server do
   context 'blob replication' do
     let(:blob_storage) { db.blob_storage }
     let(:replica_blob_storage) { db_replica.blob_storage }
-    let(:contents) { 'blob contents' }
+    let(:megabyte) { 1024 * 1024 }
+    let(:contents) { Random.new.bytes(5 * megabyte) }
     let(:blob_ref) { blob_storage.store(contents) }
 
     before do
@@ -114,7 +116,7 @@ RSpec.describe CouchbaseLite::Server do
                                                  url: "ws://localhost:#{port}/db")
 
       expect { replicator.start }.
-        to run_until { replicator.status.level == :idle }.
+        to run_until { replicator.status.level == :idle && server_replicator.status.level == :idle }.
           with_timeout(5).
           and_then {
             expect(replica_blob_storage.read(blob_ref)).to eq(contents)
