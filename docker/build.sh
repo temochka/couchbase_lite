bef9434d23a374ad02edbce5520dd85dd34d5719
Artem Chistyakov <chistyakov.artem@gmail.com>
2018-12-30 11:59:58 -0500

    Update build scripts/CI for 2.2 support.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 version=${1:-$(git ls-remote --refs https://github.com/couchbase/couchbase-lite-core.git master | cut -f1)}
+tag=${LITECORE_VERSION_STRING:-latest}
 echo "Building a docker image for cblite version $version..."
-docker build --tag temochka/cblite-test:latest --build-arg CBLITE_REF=$version .
-docker push temochka/cblite-test:latest
-
+docker build --tag temochka/cblite-test:$tag --build-arg CBLITE_REF=$version --build-arg LITECORE_VERSION_STRING=$tag .
+docker push temochka/cblite-test:$tag
